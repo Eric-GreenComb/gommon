637ca34dcee8e467d278b31f4dbe71b6faaf6e48
eric-greencomb <1121453646@qq.com>
2016-04-16 23:31:06 +0800

    add get random num

--- crypto/password.go
+++ crypto/password.go
@@ -55,3 +55,43 @@ func RandomPwd(num int) string {
 
 	return str
 }
+
+//生成随机字符串
+func GetRandomString(num int) string {
+	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	bytes := []byte(str)
+	result := []byte{}
+	r := math_rand.New(math_rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < num; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
+}
+
+const (
+	KC_RAND_KIND_NUM   = 0 // 纯数字
+	KC_RAND_KIND_LOWER = 1 // 小写字母
+	KC_RAND_KIND_UPPER = 2 // 大写字母
+	KC_RAND_KIND_ALL   = 3 // 数字、大小写字母
+)
+
+// 随机字符串
+func Krand(size int, kind int) string {
+	ikind, kinds, result := kind, [][]int{[]int{10, 48}, []int{26, 97}, []int{26, 65}}, make([]byte, size)
+	is_all := kind > 2 || kind < 0
+	math_rand.Seed(time.Now().UnixNano())
+	for i := 0; i < size; i++ {
+		if is_all { // random ikind
+			ikind = math_rand.Intn(3)
+		}
+		scope, base := kinds[ikind][0], kinds[ikind][1]
+		result[i] = uint8(base + math_rand.Intn(scope))
+	}
+	return string(result)
+}
+
+// 返回输入参数以下的随机数字，如输入参数为5，随机返回0,1,2,3,4
+func GetRandomItNum(num int) int {
+	r := math_rand.New(math_rand.NewSource(time.Now().UnixNano()))
+	return r.Intn(num)
+}
