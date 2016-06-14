5b4b91fea9eba643dbdfc3bfef00f310b7d2d454
eric-greencomb <1121453646@qq.com>
2016-06-15 00:01:23 +0800

    gommon/strings add ParseTpl

---
+++ strings/template.go
@@ -0,0 +1,21 @@
+package strings
+
+import (
+	"bytes"
+	"html/template"
+)
+
+func ParseTpl(tplname, tpl string, mmap map[string]string) string {
+	_tpl, _err := template.New(tplname).Parse(tpl)
+	if _err != nil {
+		return ""
+	}
+
+	b := bytes.NewBuffer(make([]byte, 0))
+
+	_err = _tpl.Execute(b, mmap)
+	if _err != nil {
+		return ""
+	}
+	return b.String()
+}
